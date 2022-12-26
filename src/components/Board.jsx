4e28e0ed10c1402d9e92f928864cb106ffb1a73b
JoelShajan <joelshajan11@gmail.com>
2022-12-26 22:55:23 +0530

    semi-done

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,22 +1,60 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Square } from './Square'
 function Board() {
+const [bdarr,setbd] = useState(Array(9).fill(null));
+const [isXNext,setXNext] = useState(false);
+//  const handleSqClick= position=>{
+//     setbd(prev=>{
+//       return prev.map(Square,pos) => {
+//           if(pos === position){
+//             return "X";
+//           }
+//           return  Square;
+//         });
+//     });
+//  };
+const handleSqClick = (position)=>{
+  if(bdarr[position]!=null){
+    return;
+  }
+setbd ((prev) => {
+   return prev.map((curr,pos) => {
+    
+    if (pos===position){
+      
+      return isXNext? 'X':'O';
+    }
+    return curr;
+   })
+    
+})
+setXNext((prev)=> {return !prev;});
+};
+  const rendersquare=(position)=>{
+    return (
+    <Square 
+    value={bdarr[position]}
+    onClick={()=>{handleSqClick(position)}
+    }
+/>
+    )
+  }; 
   return (
     <div className='board'>
         <div className='board-row'>
-            <Square value={0}/>
-            <Square value={1}/>
-            <Square value={2}/>
+            {rendersquare(0)}
+            {rendersquare(1)}
+            {rendersquare(2)}
         </div>
         <div className='board-row'>
-            <Square value={3}/>
-            <Square value={4}/>
-            <Square value={5}/>
+            {rendersquare(3)}
+            {rendersquare(4)}
+            {rendersquare(5)}
         </div>
         <div className='board-row'>
-            <Square value={6}/>
-            <Square value={7}/>
-            <Square value={8}/>
+            {rendersquare(6)}
+            {rendersquare(7)}
+            {rendersquare(8)}
         </div>
     </div>
   )
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-export const Square = ({value}) => {
+export const Square = ({value,onClick}) => {
   return (
-    <button className="square" type='button'>{value}</button>
+    <button className="square" type='button' onClick={onClick}>
+      {value}
+      </button>
   )
 }
